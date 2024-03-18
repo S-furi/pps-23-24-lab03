5ebc4b180ae74a7142fa90a077ae259824b967ff
S-furi <stefano.furi@studio.unibo.it>
2024-03-18 11:34:36 +0100

    feat: complete task 1

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -2,6 +2,9 @@ package u03
 
 import u02.AnonymousFunctions.l
 import u03.Optionals.Optional
+import u03.Optionals.Optional.isEmpty
+import u03.Optionals.Optional.orElse
+import u03.EncapsulatedSequences.Sequence.nil
 
 object Sequences: // Essentially, generic linkedlists
   
@@ -25,14 +28,41 @@ object Sequences: // Essentially, generic linkedlists
       case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), zip(t1, t2))
+      case _ => Nil()
 
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
+      case Cons(h, t) if n > 0 => Cons(h, take(t)(n - 1))
+      case _ => Nil()
+
+    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = (l1, l2) match
+      case (Cons(h1, t1), _) => Cons(h1, concat(t1, l2))
+      case (Nil(), Cons(h, t)) => Cons(h, t)
+      case _ => Nil()
+
+    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = l match
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
+      case _ => Nil()
     
-    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
-    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
-
-    def min(l: Sequence[Int]): Optional[Int] = ???
+    def mapFM[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = flatMap(l)(x => Cons(mapper(x), Nil()))
+    
+    def filterFM[A](l: Sequence[A])(predicate: A => Boolean): Sequence[A] = flatMap(l)(x => x match
+      case x if predicate(x) => Cons(x, Nil())
+      case _ => Nil()
+    )
+    
+    def min(l: Sequence[Int]): Optional[Int] = l match
+      case Cons(h, t) => {
+        val nextMin = min(t)
+        if (h > orElse(nextMin, Int.MaxValue)) {
+          nextMin
+        } else {
+          Optional.Just(h)
+        }
+      }
+      case _ => Optional.Empty()
+    
     
 @main def trySequences =
   import Sequences.* 
--- src/test/scala/u03/SequenceTest.scala
+++ src/test/scala/u03/SequenceTest.scala
@@ -39,9 +39,18 @@ class SequenceTest:
     val l2: Sequence[Int] = Cons(40, Cons(50, Nil()))
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Cons(50, Nil()))))), concat(l, l2))
     assertEquals(Cons(40, Cons(50, Nil())), concat(Nil(), l2))
+
   @Test def testFlatMap() =
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
     assertEquals(Nil(), flatMap(Nil())(v => Cons(v, Nil())))
+
+  @Test def testMapInTermsOfFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), mapFM(l)(_ + 1))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), mapFM(l)(_ + ""))
+
+  @Test def testFilterInTermsOfFlatMap() =
+    assertEquals(Cons(20, Cons(30, Nil())), filterFM(l)(_ >= 20))
+    assertEquals(Cons(10, Cons(30, Nil())), filterFM(l)(_ != 20))
 
   @Test def testMin() =
     assertEquals(Just(10), min(l))

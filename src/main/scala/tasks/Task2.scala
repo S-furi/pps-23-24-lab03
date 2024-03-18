509ef13f388bf8659acbcc15c24949c92cc76a49
S-furi <stefano.furi@studio.unibo.it>
2024-03-18 11:35:46 +0100

    refactor: remove external match case

--- src/main/scala/tasks/Task2.scala
+++ src/main/scala/tasks/Task2.scala
@@ -6,9 +6,7 @@ import u02.Modules.Person
 import u02.Modules.Person.*
 
 object Task2:
-    def teacherCourses(l: Sequence[Person]): Sequence[String] = l match
-        case Cons(head, tail) => flatMap(l)(x => x match
+    def teacherCourses(l: Sequence[Person]): Sequence[String] = flatMap(l)(x => x match
             case Teacher(name, course) => Cons(course, Nil())
             case _ => Nil()
         )
-        case _ => Nil()
